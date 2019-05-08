94dbdc22ba27813d1c2992dd0b24bcf1f11ae1c0
adyrathore <adyrathore@gmail.com>
2019-05-08 14:44:21 +0530

    1st commit

--- app/src/main/java/com/jskgmail/indiaskills/adpater/AdapterExamList.java
+++ app/src/main/java/com/jskgmail/indiaskills/adpater/AdapterExamList.java
@@ -127,6 +127,7 @@ public class AdapterExamList extends BaseAdapter {
                 viewHolder.btnOperation.setText(R.string.upload);
                 viewHolder.btnOperation.setEnabled(false);
             } else {
+
                 viewHolder.ivTestStatus.setVisibility(View.GONE);
                 viewHolder.btnOperation.setText(R.string.take_test);
                 viewHolder.btnOperation.setEnabled(true);
